48f93913ac346eebc7897a133d9e64e161e48421
Ayaka Ikezawa <ikezawa.0728@gmail.com>
2012-05-03 15:25:40 +0900

    add redis to app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
+require 'open-uri'
+
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 get '/' do
-  File.open("likes.json") do |f|
-    f.read
-  end
+  REDIS.get("like")
 end
